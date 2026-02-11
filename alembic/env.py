fc7dc3eac2357119eec5fb67e104a2bd4a90dc5e
isli18 <islikr2005@gmail.com>
2026-02-11 20:07:53 +0100

    changes: made changes to adapt the project to Windows

--- alembic/env.py
+++ alembic/env.py
@@ -58,15 +58,11 @@ def run_migrations_offline() -> None:
 
 
 def run_migrations_online() -> None:
-    """Run migrations in 'online' mode.
+    """Run migrations in 'online' mode."""
 
-    In this scenario we need to create an Engine
-    and associate a connection with the context.
-
-    """
     configuration = config.get_section(config.config_ini_section)
     configuration["sqlalchemy.url"] = get_url()
-    
+
     connectable = engine_from_config(
         configuration,
         prefix="sqlalchemy.",
@@ -75,7 +71,10 @@ def run_migrations_online() -> None:
 
     with connectable.connect() as connection:
         context.configure(
-            connection=connection, target_metadata=target_metadata
+            connection=connection,
+            target_metadata=target_metadata,
+            compare_type=True,
+            render_as_batch="sqlite" in get_url(),  # critical for SQLite
         )
 
         with context.begin_transaction():
